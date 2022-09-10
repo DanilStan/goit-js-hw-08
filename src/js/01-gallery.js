81031736cc8fe652909c4984c2bbbfdf0106ad29
DanilStan <stanislavov.danik@gmail.com>
2022-09-10 18:57:07 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const allColection = document.querySelector('.gallery');
+const createImagesColectionToHtml = createImagesColection(galleryItems);
+
+allColection.insertAdjacentHTML('beforeend', createImagesColectionToHtml);
+//  Формуємо HTML
+function createImagesColection(values) {
+  return values
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}"   />
+              </a>`;
+    })
+    .join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  /* options */
+});
+
+lightbox.captionPosition = 'bottom';
+lightbox.captionDelay = 250;
